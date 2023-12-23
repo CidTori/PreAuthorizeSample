7749130402204d4e1ed9c8760d79aa8147fee304
Cédric Thireau <cedric.thireau@hotmail.fr>
2023-12-23 16:36:23 +0100

    Init

---
+++ src/main/java/com/example/demo/TestController.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+    @GetMapping("/test")
+    @PreAuthorize("permitAll()")
+    public void test() {
+    }
+}
